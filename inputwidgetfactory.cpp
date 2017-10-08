f35808b5aad4dc629c3ea6d84c208e1dce2a6ab4
Skycoder42 <barz.felix@web.de>
2017-10-08 18:17:34 +0200

    extended input factory

--- inputwidgetfactory.cpp
+++ inputwidgetfactory.cpp
@@ -6,11 +6,17 @@
 #include <QLineEdit>
 #include <QSpinBox>
 
+InputWidgetFactory::InputWidgetFactory() :
+	_simpleWidgets()
+{}
+
 InputWidgetFactory::~InputWidgetFactory() {}
 
 QWidget *InputWidgetFactory::createWidget(const QByteArray &type, QWidget *parent, const QVariantMap &editProperties)
 {
 	QWidget *widget = nullptr;
+	if(_simpleWidgets.contains(type))
+		widget = _simpleWidgets.value(type)(parent);
 	if(type == "bool")
 		widget = new QCheckBox(parent);
 	else if(type == "string" || type == "QString")
@@ -34,11 +40,18 @@ QMetaProperty InputWidgetFactory::userProperty(QWidget *widget)
 	return widget->metaObject()->userProperty();
 }
 
+bool InputWidgetFactory::addSimpleWidget(const QByteArray &type, const std::function<QWidget *(QWidget *)> &creator)
+{
+	Q_ASSERT_X(creator, Q_FUNC_INFO, "The passed creation function must be valid");
+	_simpleWidgets.insert(type, creator);
+	return true;
+}
+
 QWidget *InputWidgetFactory::createListWidget(QWidget *parent, const QVariantMap &editProperties)
 {
 	auto box = new ListComboBox(parent);
 	foreach(auto item, editProperties.value("_list_elements").toList()) {
-		if(item.type() == QMetaType::QString)
+		if(item.type() == QVariant::String)
 			box->addItem(item.toString(), item);
 		else {
 			auto iData = item.toMap();
--- inputwidgetfactory.h
+++ inputwidgetfactory.h
@@ -3,17 +3,33 @@
 
 #include <QMetaProperty>
 #include <QWidget>
+#include <functional>
 
 class InputWidgetFactory
 {
 public:
+	InputWidgetFactory();
 	virtual ~InputWidgetFactory();
 
 	virtual QWidget *createWidget(const QByteArray &type, QWidget *parent, const QVariantMap &editProperties);
 	virtual QMetaProperty userProperty(QWidget *widget);
 
+	virtual bool addSimpleWidget(const QByteArray &type, const std::function<QWidget*(QWidget*)> &creator);
+	template <typename TWidget>
+	bool addSimpleWidget(const QByteArray &type);
+
 protected:
 	virtual QWidget *createListWidget(QWidget *parent, const QVariantMap &editProperties);
+
+	QHash<QByteArray, std::function<QWidget*(QWidget*)>> _simpleWidgets;
 };
+
+template<typename TWidget>
+bool InputWidgetFactory::addSimpleWidget(const QByteArray &type)
+{
+	return addSimpleWidget(type, [](QWidget *parent){
+		return new TWidget(parent);
+	});
+}
 
 #endif // INPUTWIDGETFACTORY_H
